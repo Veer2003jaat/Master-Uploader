ff165649d618bf828326719821a20406eccc3439
काला बिच्छू 🦂 <jaiveer2003godara@gmail.com>
2025-04-30 21:46:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USER = os.environ.get('AUTH_USERS', '7621154046').split(',')
+    BOT_TOKEN = os.environ.get("7610944579:AAHG3CTF9zWo2mRGXuXpRhcGdKkTXU1m6Qc")
+    API_ID = int(os.environ.get("12475131"))
+    API_HASH = os.environ.get("719171e38be5a1f500613837b79c536f")
+    AUTH_USER = os.environ.get('AUTH_USERS', '5121141243').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     HOST = "https://drm-api-six.vercel.app"
     CREDIT = "[꧁ 𝐉𝐨𝐡𝐧 𝐖𝐢𝐜𝐤 ꧂](https://t.me/Dc5txt_bot)"
